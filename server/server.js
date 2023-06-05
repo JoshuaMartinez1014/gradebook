fa31f1b1c5fd8babf8a3a6b1b228e3974f4ea313
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-06-05 12:41:07 -0500

    connection file

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const db = require("./config/connection");
-/* const routes = require('./routes'); */
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
