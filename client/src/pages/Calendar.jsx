328646787fa082f89fd59108730cff2893accd5c
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-06-07 03:07:06 -0500

    added stylizing to calender

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -1,15 +1,15 @@
-import "@fullcalendar/core/main.css";
-import "@fullcalendar/daygrid/main.css";
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import "../styles/global.css";
 
 function Calendar() {
   return (
-    <div style={{ width: "80%", paddingTop: "30px" }}>
+    <div style={{ width: "80%", height: "100%", paddingTop: "30px" }}>
       <FullCalendar
         plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
+        height="100%"
         events={[
           { title: "event 1", date: "2023-06-01" },
           { title: "event 2", date: "2023-06-02" },
