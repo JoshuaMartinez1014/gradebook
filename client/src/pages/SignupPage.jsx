5f1231c16d8464f0ae4f3ce914bc148f8eb64ca7
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-06-07 10:51:18 -0500

    signup form setup changes, root package update

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -3,35 +3,52 @@ import { Container, Form, Button } from "react-bootstrap";
 
 function Signup() {
   const [formData, setFormData] = useState();
-  function handleChange (e) {
-    setFormData({...formData, [e.target.name]: e.target.value})
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   }
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault();
 
-    fetch('http://localhost:3001/api/user', {
-      method: 'POST',
+    fetch("http://localhost:3001/api/user", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData)
-    }).then(response => response.json()).then(data=> {
-      console.log(data)
+      body: JSON.stringify(formData),
     })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
   }
   return (
     <>
       <div style={{ marginTop: "50px" }}>
         <h1 style={{ marginLeft: "10%" }}>Signup</h1>
         <Container className="d-flex justify-content-center mt-3">
-          <Form className="mt-3" style={{ width: "800px" }} onSubmit={handleSubmit}>
+          <Form
+            className="mt-3"
+            style={{ width: "800px" }}
+            onSubmit={handleSubmit}
+          >
             <Form.Group className="mb-3">
-              <Form.Label>UserName</Form.Label>
+              <Form.Label>First Name</Form.Label>
               <Form.Control
                 type="text"
-                name="username"
+                name="fname"
                 onChange={handleChange}
-                placeholder="Enter your username"
+                placeholder="Enter your first name"
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="lname"
+                onChange={handleChange}
+                placeholder="Enter your last name"
                 required
               />
             </Form.Group>
@@ -54,6 +71,17 @@ function Signup() {
                 name="password"
                 onChange={handleChange}
                 placeholder="Enter your Password"
+                required
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                onChange={handleChange}
+                placeholder="Enter your email"
                 required
               />
             </Form.Group>
