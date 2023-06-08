a9227abc6967138ac6cb215dd42b04d9aabfba31
DockDock <ndockman@comcast.net>
2023-06-08 10:39:47 -0500

    Initial UserContext.js.

--- client/src/ctx/UserContext.js
+++ client/src/ctx/UserContext.js
@@ -1,2 +1,51 @@
-/* import { createContext, useContext, useState, useEffect } from "react";
-import Cookies from "js-cookie"; */
+import { createContext, useContext, useState, useEffect } from "react";
+import Cookies from "js-cookie";
+
+
+const UserContext = createContext({});
+export const useUserContext = () => useContext(UserContext)
+
+export const UserProvider = ({ children }) => {
+    //.... define our state variables...
+    const [ currUser, setCurrUser ] = useState(null)
+
+    const verifyUser = async() => {
+        if( Cookies.get("auth-cookie") ){
+          try {
+            const query = await fetch("/api/user/verify", {
+              method: "post",
+              body: JSON.stringify({}),
+              headers: {
+                "Content-Type": "application/json"
+              }
+            })
+            const result = await query.json()
+            if( result && result.status === "success" ){
+              setCurrUser(result.payload)
+            }
+          } catch(err){
+            if( !window.location.href.includes("/login") ){
+              window.location.href = "/login"
+            }
+          }
+        }
+      }
+
+      const logout = () => {
+        Cookies.remove("auth-cookie");
+        window.location.href = "/login"
+      }
+    
+      useEffect(() => {
+        console.log("verifying user")
+        verifyUser()
+      }, [window.location.href])
+
+
+
+    return (
+        <UserContext.Provider value={{ currUser, logout }}>
+            { children }
+        </UserContext.Provider>
+    )
+}
