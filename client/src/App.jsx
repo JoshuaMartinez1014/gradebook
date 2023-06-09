c19d6b879e5e49ea8c4fed9ce0119b440d6f5aa1
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-06-09 10:33:13 -0500

    stash changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,27 +26,27 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        {/*  <UserProvider> */}
-        <div id="page-container">
-          <Header />
-          <div id="page-main">
-            <SideNav />{" "}
-            <div id="page-content" style={{}}>
-              <Routes>
-                <Route path="/" exact element={<HomePage />} />
-                <Route path="/login" exact element={<LoginPage />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/grades" element={<GradePage />} />
-                <Route path="/syllabus" element={<SyllabusPage />} />
-                <Route path="/teacher" element={<TeacherPage />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/assignments" element={<AssignmentsPage />} />
-                <Route path="/assignment/:id" element={<AssignmentPage />} />
-              </Routes>{" "}
+        <UserProvider>
+          <div id="page-container">
+            <Header />
+            <div id="page-main">
+              <SideNav />{" "}
+              <div id="page-content" style={{}}>
+                <Routes>
+                  <Route path="/" exact element={<HomePage />} />
+                  <Route path="/login" exact element={<LoginPage />} />
+                  <Route path="/signup" element={<SignupPage />} />
+                  <Route path="/grades" element={<GradePage />} />
+                  <Route path="/syllabus" element={<SyllabusPage />} />
+                  <Route path="/teacher" element={<TeacherPage />} />
+                  <Route path="/calendar" element={<Calendar />} />
+                  <Route path="/assignments" element={<AssignmentsPage />} />
+                  <Route path="/assignment/:id" element={<AssignmentPage />} />
+                </Routes>{" "}
+              </div>
             </div>
           </div>
-        </div>
-        {/*  </UserProvider> */}
+        </UserProvider>
       </BrowserRouter>
     </>
   );
