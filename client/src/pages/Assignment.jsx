06fe637ef5628463fd14519c1a0974d7af5f4b12
Sean Hayes <hayessea000@outlook.com>
2023-06-10 16:49:23 -0500

    changed api call

--- client/src/pages/Assignment.jsx
+++ client/src/pages/Assignment.jsx
@@ -13,25 +13,30 @@ function AssignmentPage() {
   const [submitedData, setSubmitedData] = useState(false);
   const [assignmentData, setAssignmentData] = useState("");
 
+  async function lookupSubmited() {
+    const apiPath = `/api/submited`
+    const searchQuery = await fetch(apiPath)
+    const results = await searchQuery.json()
+    console.log(results)
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].student === currUser._id && results[i].assignment === url) {
+        setSubmitedData(true)
+      }
+    }
+    setAssignmentData([results.assignment_name, results._id])
+  }
   async function lookupAssignment() {
     const apiPath = `/api/assignment/${url}`
     const searchQuery = await fetch(apiPath)
     const results = await searchQuery.json()
     console.log(results)
-    for(let i =0; i <results.grade.length; i++){
-        if(results.grade[i].student===currUser._id){
-            setSubmitedData(true)
-            console.log("is true")
-        }else {
-            setSubmitedData(false)
-            console.log("is false")
-        }
-    }
+
     setAssignmentData([results.assignment_name, results._id])
   }
   useEffect(() => {
     console.log("context use effect working");
-    if( currUser && currUser._id ){
+    if (currUser && currUser._id) {
+      lookupSubmited();
       lookupAssignment();
     }
   }, [currUser]);
@@ -39,7 +44,7 @@ function AssignmentPage() {
     <div style={{ marginTop: "50px", width: "800px" }}>
       <h1 style={{ marginLeft: "10%" }}>Assignment</h1>
       <br />
-      {submitedData ?(<SubmitedAssignment setPosted={setSubmitedData} data={assignmentData}/>):(<NotSubmitedAssignment setPosted={setSubmitedData} data={assignmentData}/>)}
+      {submitedData ? (<SubmitedAssignment setPosted={setSubmitedData} data={assignmentData} />) : (<NotSubmitedAssignment setPosted={setSubmitedData} data={assignmentData} />)}
     </div>
   );
 }
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,9 +1,10 @@
 const db = require('../config/connection');
-const { User, Grade, Class, Assignment } = require('../models');
+const { User, Grade, Class, Assignment, Submited } = require('../models');
 const userSeed = require('./userSeed.json')
 const classSeed = require('./classSeed.json')
 const assignmentSeed = require('./assignmentSeed.json')
 const gradeSeed = require('./gradeSeed.json')
+const submitedSeed = require('./submitedSeed.json')
 
 db.once('open', async () => {
   try {
@@ -13,6 +14,8 @@ db.once('open', async () => {
     await Class.create(classSeed);
     await Assignment.deleteMany({});
     await Assignment.create(assignmentSeed);
+    await Submited.deleteMany({});
+    await Submited.create(submitedSeed);
     await Grade.deleteMany({});
     await Grade.create(gradeSeed);
 
